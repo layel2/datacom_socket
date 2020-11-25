cb936704b2ec1c76f66425397d12dba682c2ebf0
layel2 <chaos_layyer@live.com>
2020-11-25 16:21:41 +0700

    update

--- socket-test-client.py
+++ socket-test-client.py
@@ -14,7 +14,7 @@ def load_key():
 # Create a socket object 
 s = socket.socket()				
 
-IS_ENCRYPT = True
+IS_ENCRYPT = False
 PACKGATE_LEN = 2048 #bytes
 FRAGMENT_HEADER_SIZE = 2 #bytes
 CHECKSUM_SIZE = 16 
@@ -23,7 +23,7 @@ DATA_SIZE = PACKGATE_LEN - FRAGMENT_HEADER_SIZE - CHECKSUM_SIZE
 
 # connect to the server
 port = 1234
-s.connect(('192.168.1.50', port)) 
+s.connect(('192.168.1.2', port)) 
 get_data = []
 rev_pkg = s.recv(PACKGATE_LEN)
 get_data.append(rev_pkg)
@@ -55,12 +55,12 @@ print("Recive md5 is ",rev_checksum)
 msg_checksum = hashlib.md5(rev_msg).digest()
 print("Checksum match :",msg_checksum==rev_checksum)
 
-#key = b'uwSs-EoXsrgAeZj9MVB_Rfm1kwlooP6Mwddm9iCmh5c='
-key = load_key()
+key = b'uwSs-EoXsrgAeZj9MVB_Rfm1kwlooP6Mwddm9iCmh5c='
+#key = load_key()
 f = Fernet(key)
 if(IS_ENCRYPT):
     decrypt_data = f.decrypt(rev_msg)
-print("Decrypt message is ",decrypt_data.decode('utf-8'))
+    print("Decrypt message is ",decrypt_data.decode('utf-8'))
 
 
 
--- socket-test-server.py
+++ socket-test-server.py
@@ -17,13 +17,17 @@ def load_key():
     """
     return open("secret.key", "rb").read()
 
-IS_ENCRYPT = True
+IS_ENCRYPT = False
 CHECKSUM_SIZE = 16 #MD5
-FRAGMENT_NUM = 2
+FRAGMENT_NUM = 3
 FRAGMENT_HEADER_SIZE = 2 #bytes
 
+IPREV_1 = '192.168.1.1'
+IPREV_2 = '192.168.1.2'
+
+
 port = 1234
-s.bind(('192.168.1.50', port))		 
+s.bind(('192.168.1.3', port))		 
 print ("socket binded to %s" %(port) )
 
 # put the socket into listening mode 
@@ -33,29 +37,43 @@ print ("socket is listening")
 
 #read_data = open('4thai.png','rb').read()
 #encryption
-#key = b'uwSs-EoXsrgAeZj9MVB_Rfm1kwlooP6Mwddm9iCmh5c='
-key = load_key()
+key = b'uwSs-EoXsrgAeZj9MVB_Rfm1kwlooP6Mwddm9iCmh5c='
+#key = load_key()
 f = Fernet(key)
 
-text = "Hello world my name is Pranpaveen Lay. เลย์ๆ"
+text = "Pranpaveen Laykaviriyakul Lay Hello world eiei wow za1234567890"
 data = text.encode('utf-8')
 if(IS_ENCRYPT):
     data = f.encrypt(data)
 checksum = hashlib.md5(data).digest()
 
 combine_data = data+checksum
-fragment_size = int(len(combine_data)/2 + 0.5)
+fragment_size = int(len(combine_data)/FRAGMENT_NUM + 0.5)
 
+data_frag = []
+for i in range(FRAGMENT_NUM):
+    #print(i)
+    frag_head = (str(i+1)+str(FRAGMENT_NUM)).encode('utf-8')
+    frag_data = (combine_data[i*fragment_size:(i+1)*fragment_size])
+    data_frag.append(frag_head+frag_data)
+    #time.sleep(0.001)
 
 while True: 
 
     c, addr = s.accept()	 
     print ('Got connection from', addr )
+    '''
+    if addr[0] == IPREV_1 :
+        c.send(data_frag[0])
+    if addr[0] == IPREV_2 :
+        c.send(data_frag[1])
+    '''
     for i in range(FRAGMENT_NUM):
         #print(i)
         frag_head = (str(i+1)+str(FRAGMENT_NUM)).encode('utf-8')
         frag_data = (combine_data[i*fragment_size:(i+1)*fragment_size])
         c.send(frag_head+frag_data)
-        time.sleep(0.001)
+        time.sleep(0.1)
+    
 
     c.close() 
